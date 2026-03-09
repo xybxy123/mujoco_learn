f2f106926b3c72ce7fc1bd6c36375db212c40612
xybxy123 <2365421405@qq.com>
2026-03-09 14:42:02 +0800

    edit dog_size

---
+++ Chapter3_ctrl/build/CMakeFiles/mj_ctrl.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xybxy123/w_work/mujoco_learn/Chapter3_ctrl/src/leg_ik.c" "CMakeFiles/mj_ctrl.dir/src/leg_ik.c.o" "gcc" "CMakeFiles/mj_ctrl.dir/src/leg_ik.c.o.d"
+  "/home/xybxy123/w_work/mujoco_learn/Chapter3_ctrl/src/main.cpp" "CMakeFiles/mj_ctrl.dir/src/main.cpp.o" "gcc" "CMakeFiles/mj_ctrl.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
